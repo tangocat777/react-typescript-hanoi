89f5ec01051afac5d35002b24f684ec1ef610688
mitchellmartin776 <mitchell.martin776@gmail.com>
2024-12-29 20:18:40 -0500

    Make npm start work

---
+++ react-typescript-hanoi/src/Stack.ts
@@ -0,0 +1,27 @@
+class Stack {
+    private internalArray: Number[];
+    // Private array to store stack elements
+
+    constructor() {
+        this.internalArray = [];
+    }
+
+    push(element: Number): void {
+        this.internalArray.push(element);
+    }
+
+    pop(): Number | undefined {
+        return this
+            .internalArray.pop();
+    }
+
+    peek(): Number | undefined {
+        return this
+            .internalArray[this.internalArray.length - 1];
+    }
+
+    size(): Number {
+        return this
+            .internalArray.length;
+    }
+}
